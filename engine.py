1685317b957a26a2a24c1c73d8d26cb4a4b71b26
tcum7 <137916784+tcum7@users.noreply.github.com>
2025-11-20 12:06:27 -0500

    Formatting

--- engine.py
+++ engine.py
@@ -29,6 +29,9 @@ class Engine:
         self.v_inf = self.c_inf*M
 
     def inlet(self):
+        '''
+        Inlet Properties: Stagnation Pressure & Temperature
+        '''
         gamma = 1.4
         T_o_inlet = self.T_a*(1+gamma/2*self.M**2)
         if self.M<1:
@@ -40,6 +43,9 @@ class Engine:
         return P_o_inlet,T_o_inlet
 
     def fan(self,P,T):
+        '''
+        Fan Properties: Stagnation Pressure & Temperature, Drag, and Work
+        '''
         gamma = 1.4
         eff_fan = (self.Prf**((gamma-1)/gamma)-1)/(self.Prf**((gamma-1)/(gamma*0.90))-1)
         C_beta_1 = 0.245*1000
@@ -52,6 +58,9 @@ class Engine:
         return P_o_fan,T_o_fan,delta_drag, work
 
     def compressor(self,P_o,T_o):
+        '''
+        Compressor Properties: Stagnation Pressure & Temperature, and Work
+        '''
         gamma = 1.38
         eff_c = (self.Prc**((gamma-1)/gamma)-1)/(self.Prc**((gamma-1)/(gamma*0.90))-1)
         T_o_c = T_o*(1+1/eff_c*((self.Prc)**((gamma-1)/gamma)-1))
@@ -61,6 +70,9 @@ class Engine:
         return P_o_c,T_o_c, work
     
     def burner(self,P_o,T_o):
+        '''
+        Burner Properties: Stagnation Pressure & Temperature
+        '''
         gamma = 1.33
         eff_burner = 0.99
         Cp_burner = gamma*self.R_uni/28.8/(gamma-1)
@@ -70,6 +82,9 @@ class Engine:
         return T_o_burner, P_o_burner
     
     def turbine(self,P_o,T_o,work):
+        '''
+        Turbine Properties: Stagnation Pressure & Temperature, and the max Temperature
+        '''
         gamma = 1.33
         Cp_turbine = gamma*self.R_uni/28.8/(gamma-1)
         T_o_turbine = T_o-work/(Cp_turbine(1+self.f))
@@ -82,12 +97,18 @@ class Engine:
         return P_o_turbine,T_o_turbine,Tmax
     
     def turbinemixer(self,P_o_turbine,T_o_turbine,T_o_compressor):
+        '''
+        Turbine Mixer Properties: Stagnation Pressure & Temperature
+        '''
         gamma=1.34
         T_o_tm = (T_o_turbine*(1-self.b) + T_o_compressor*self.b)
         P_o_tm = (T_o_tm*T_o_compressor**(-self.b)*T_o_turbine**(self.b-1))**(gamma/(gamma-1))*P_o_turbine*P_o_turbine**(self.b)
         return P_o_tm, T_o_tm
     
     def afterburner(self,P_o,T_o):
+        '''
+        Afterburner Properties: Stagnation Pressure & Temperature
+        '''
         gamma = 1.32
         Q = 45*10**6
         Cp_ab = gamma*self.R_uni/28.8/(gamma-1)
@@ -96,6 +117,9 @@ class Engine:
         return P_o_ab,T_o_ab
     
     def corenozzle(self,P_o,T_o):
+        '''
+        Core Nozzle Properties: Exit Temperature and Velocity
+        '''
         gamma = 1.35
         Cp_cN = gamma*self.R_uni/28.8/(gamma-1)
         T_e = T_o*(1-0.95*(1-(self.P_a/P_o)**((gamma-1)/gamma)))
@@ -103,6 +127,9 @@ class Engine:
         return T_e,u_e
     
     def fannozzle(self,P_o,T_o):
+        '''
+        Fanno Nozzle Properties: Exit Temperature and Velocity
+        '''
         gamma = 1.4
         Cp_cN = gamma*self.R_uni/28.8/(gamma-1)
         T_e = T_o*(1-0.95*(1-(self.P_a/P_o)**((gamma-1)/gamma)))
@@ -110,7 +137,9 @@ class Engine:
         return T_e,u_e
     
     def combinednozzle(self,P_o_core,T_o_core,P_o_fan,T_o_fan):
-        
+        '''
+        Combined Nozzle Properties: Exit Temperature and Velocity, and Stagnation Pressure
+        '''
         T_o_mix = (T_o_core+T_o_fan*self.Beta)/(1+self.Beta)
         gamma_mix = 1.44-1.39*10**(-4)*T_o_mix+3.57*10**(-8)*T_o_mix**2
         P_o_mix = 0.80*(T_o_mix*T_o_fan**(-self.Beta/(1+self.Beta))*T_o_core**(-1/(1+self.Beta)))**(gamma_mix/(gamma_mix-1))*P_o_core*P_o_fan**(self.Beta/(self.Beta+1))
@@ -121,6 +150,9 @@ class Engine:
         return T_e,P_o_mix,u_e
     
     def fuelpump(self,P_o_burner):
+        '''
+        Fuel Pump Properties: Stagnation Pressure and Work
+        '''
         rho_f = 780
         P_o_inlet = 104000
         P_o_e = 550000+P_o_burner
