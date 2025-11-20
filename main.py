b023b50d43ba7e7fb5551065f11bb52adb25642d
tcum7 <137916784+tcum7@users.noreply.github.com>
2025-11-20 12:11:32 -0500

    Testing Engine class

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import math
 from engine.py import Engine
+
+T_a = 220 # Kelvin
+P_a = 10.0 # kPa
+M = 1.50
+Prc = 30
+Prf = 1.2
+Beta = 2
+b = 0.1
+f = 0.018
+f_ab = 0.010
+
+turbofan_engine = Engine(T_a, P_a, M, Prc, Prf, Beta, b, f, f_ab)
+
+[P_o_inlet, T_o_inlet] = turbofan_engine.inlet
+print P_o_inlet, T_o_inlet
